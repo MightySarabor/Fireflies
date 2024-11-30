d8bf663ff6eeec53ccd6951f6e16d763c441982a
MightySarabor <babaruschi@gmail.com>
2024-11-30 19:22:16 +0100

    asjdls

--- src/main/resources/firefly.thrift
+++ src/main/resources/firefly.thrift
@@ -1,5 +1,6 @@
 namespace java firefly
 
 service FireflyService {
-  void sendState(1: i32 clientId, 2: i32 state),
+    void sendState(1: i32 clientId, 2: i32 state),
+    list<i32> getNeighborStates(1: i32 gridX, 2: i32 gridY),
 }
